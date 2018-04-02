26ca0c6c06d01218b8dc67e6df5df300932ecdc9
Daniel Afonso <daniel-af@outlook.com>
2018-04-02 21:10:36 +0100

    Unique

--- migrations/20180327161511_create_user_table.js
+++ migrations/20180327161511_create_user_table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('user', function (t) {
         t.increments('id').primary()
-        t.string('username').notNullable()
+        t.string('username').unique().notNullable()
         t.string('password').notNullable()
         t.timestamps(false, true)
       })
